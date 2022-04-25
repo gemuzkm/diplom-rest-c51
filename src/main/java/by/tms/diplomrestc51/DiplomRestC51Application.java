d6bd440b36df3f845a5af8fd764ba3c6cdb7ca2a
gemuz_work <gemuzkm@gmail.com>
2022-04-25 13:09:12 +0300

    edit .gitignore

---
+++ src/main/java/by/tms/diplomrestc51/DiplomRestC51Application.java
@@ -0,0 +1,13 @@
+package by.tms.diplomrestc51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomRestC51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomRestC51Application.class, args);
+    }
+
+}
