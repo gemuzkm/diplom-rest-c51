21e28e193280d4aafc2528bf8f84f2b4a2f547cf
gemuz_work <gemuzkm@gmail.com>
2022-04-26 11:21:23 +0300

    add validation for fields

--- src/main/java/by/tms/diplomrestc51/entity/User.java
+++ src/main/java/by/tms/diplomrestc51/entity/User.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @AllArgsConstructor
@@ -16,6 +18,21 @@ public class User {
     @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Size(min = 3, max = 25)
+    private String username;
+
+    private String password;
+
+    @Size(min = 2, max = 25)
+    private String firstName;
+
+    @Size(min = 2, max = 25)
+    private String lastName;
+
+    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    private String email;
+
+    @Pattern(regexp = "^(\\+)?(\\(\\d{2,3}\\) ?\\d|\\d)(([ \\-]?\\d)|( ?\\(\\d{2,3}\\) ?)){5,12}\\d$")
+    private String phone;
 
 }
