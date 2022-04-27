0737d5dd8eed45975b55f27041f4d93ec706f7d5
gemuz_work <gemuzkm@gmail.com>
2022-04-27 12:32:31 +0300

    edit RoleRepository edit UserRepository

---
+++ src/main/java/by/tms/diplomrestc51/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package by.tms.diplomrestc51.repository;
+
+import by.tms.diplomrestc51.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/by/tms/diplomrestc51/service/UserService.java
@@ -0,0 +1,49 @@
+package by.tms.diplomrestc51.service;
+
+import by.tms.diplomrestc51.entity.Role;
+import by.tms.diplomrestc51.entity.User;
+import by.tms.diplomrestc51.enums.UserStatus;
+import by.tms.diplomrestc51.repository.RoleRepository;
+import by.tms.diplomrestc51.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    public void registration(UserDTO userDTO) {
+        User user = UserConverter.convertToUserFromUserSignupDTO(userDTO);
+        List<Role> roles = new ArrayList<>();
+        Role role = new Role();
+        role.setTypeOfRole("USER");
+        roles.add(role);
+        user.setRoleList(roles);
+        user.setStatus(UserStatus.ACTIVE);
+        role.setUser(user);
+        userRepository.save(user);
+        roleRepository.save(role);
+    }
+
+    public User findByUsername(String username) {
+        User byUsername = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User with username: " + username + " not found"));
+        return byUsername;
+    }
+
+    public boolean existByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    public boolean existByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+}
