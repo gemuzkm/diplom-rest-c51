ba7802058d6547089ea437107f9ad18b5cddbeac
gemuz_work <gemuzkm@gmail.com>
2022-04-27 10:42:43 +0300

    add jwt config add jwt exception

---
+++ src/main/java/by/tms/diplomrestc51/exception/JWTAuthenticationException.java
@@ -0,0 +1,14 @@
+package by.tms.diplomrestc51.exception;
+
+import javax.naming.AuthenticationException;
+
+public class JWTAuthenticationException extends AuthenticationException {
+    public JWTAuthenticationException(String msg) {
+        super(msg);
+    }
+
+   public JWTAuthenticationException() {
+        super();
+    }
+
+}
