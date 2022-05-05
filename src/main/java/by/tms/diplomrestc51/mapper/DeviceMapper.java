10c3ec56a346af5b8a7e3b7773a08859af2d6798
gemuz_work <gemuzkm@gmail.com>
2022-05-05 09:09:34 +0300

    add mapping device to VacuumCleanerDevice

--- src/main/java/by/tms/diplomrestc51/mapper/DeviceMapper.java
+++ src/main/java/by/tms/diplomrestc51/mapper/DeviceMapper.java
@@ -2,9 +2,9 @@ package by.tms.diplomrestc51.mapper;
 
 import by.tms.diplomrestc51.entity.Device;
 import by.tms.diplomrestc51.entity.device.RefrigeratorDevice;
+import by.tms.diplomrestc51.entity.device.VacuumCleanerDevice;
 import by.tms.diplomrestc51.entity.device.WasherDevice;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Component;
 public interface DeviceMapper {
     WasherDevice deviceToWasherDevice(Device device);
     RefrigeratorDevice deviceToRefrigeratorDevice(Device device);
+    VacuumCleanerDevice deviceToVacuumCleanerDevice(Device device);
 }
