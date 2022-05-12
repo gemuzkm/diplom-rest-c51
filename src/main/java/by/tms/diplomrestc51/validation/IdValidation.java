906b5593b46597b8d757cebaad3c9c4d3c059317
gemuz_work <gemuzkm@gmail.com>
2022-05-12 09:37:33 +0300

    add test IdValidation

--- src/main/java/by/tms/diplomrestc51/validation/IdValidation.java
+++ src/main/java/by/tms/diplomrestc51/validation/IdValidation.java
@@ -3,7 +3,6 @@ package by.tms.diplomrestc51.validation;
 import by.tms.diplomrestc51.exception.InvalidException;
 
 public class IdValidation {
-
     public static boolean validate(long id) {
         if (id < 1) {
             throw new InvalidException("Invalid id");
