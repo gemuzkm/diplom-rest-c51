34042b049c4248c37538b409e94106e4eea2a3d9
gemuz_work <gemuzkm@gmail.com>
2022-05-20 13:49:40 +0300

    add flyway

---
+++ src/main/java/by/tms/diplomrestc51/configuration/FlyWayConfiguration.java
@@ -0,0 +1,21 @@
+package by.tms.diplomrestc51.configuration;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Configuration
+public class FlyWayConfiguration {
+    @Bean
+    FlywayMigrationInitializer flywayInitializer(Flyway flyway) {
+        return new FlywayMigrationInitializer(flyway, (f) ->{} );
+    }
+
+    @Bean
+    @DependsOn("entityManagerFactory")
+    FlywayMigrationInitializer delayedFlywayInitializer(Flyway flyway) {
+        return new FlywayMigrationInitializer(flyway);
+    }
+}
